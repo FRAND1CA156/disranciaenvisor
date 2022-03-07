d6c080ab54591eec64a68d58963ba587a8b982e5
FRAND1CA156 <99391663+FRAND1CA156@users.noreply.github.com>
2022-03-07 09:47:47 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-
+let distancia = 0
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+    distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
+    if (distancia < 9) {
+        basic.showString("" + (distancia))
+    } else {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    maqueen.motorStop(maqueen.Motors.All)
+})

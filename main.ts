758c3c43faf7465ea7fbf8fc5aa9cbc4f111a92d
FRAND1CA156 <99391663+FRAND1CA156@users.noreply.github.com>
2022-03-10 10:12:53 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
 let distancia = 0
 basic.forever(function () {
     distancia = maqueen.Ultrasonic(PingUnit.Centimeters)
-    if (distancia < 20) {
+    basic.showString("" + (distancia))
+    if (distancia < 60) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 15)
+        basic.pause(500)
+    }
+    if (distancia < 25) {
+        maqueen.motorStop(maqueen.Motors.All)
         basic.showString("" + (distancia))
-        if (distancia < 10) {
-            basic.showString("" + (distancia))
-            maqueen.motorStop(maqueen.Motors.All)
-            basic.pause(5000)
-        }
-    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 59)
+        maqueen.motorStop(maqueen.Motors.M2)
+        basic.pause(5000)
         basic.showLeds(`
             # . # . #
-            . # # # .
-            # # # # #
-            . # # # .
+            . # . # .
+            # . # . #
+            . # . # .
             # . # . #
             `)
     }
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
 })
